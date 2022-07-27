ac4e4309e74780cf701ba792187d90a24f18e2a0
Nartay Kasim <kasimnartay@gmail.com>
2022-07-27 13:34:21 -0500

    changed back

--- index.js
+++ index.js
@@ -45,10 +45,12 @@ app.get(
 app.put("/api/inventory/item/update", itemController.updateItem);
 app.post("/api/inventory/item/note", itemController.createNote);
 
-app.use(express.static("client/build"));
-app.get("*", (req, res) => {
-   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+   app.use(express.static("client/build"));
+   app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+   });
+}
 
 // app.use((req, res, next) => {
 //    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
